84eaa84490f6f39e06d47084e9d88043fce5aac3
soda-flavour <axa8380@gmail.com>
2020-08-05 03:49:06 +0900

    Tab 1 Main Screen header

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,22 @@ import 'package:provider/provider.dart';
 import 'package:counseling_flutter/src/helper/AppConfig.dart';
 import 'package:counseling_flutter/pages/BottomNaviController.dart';
 
+const AppColors = {
+  "appbar_title": Color(0xff000000),
+  "appbar_background": Color(0xffffffff),
+  "appbar_icon": Color(0xff000000),
+  "appbar_border": Color(0xffE2E2E2),
+  "btn_border_gray": Color(0xffE2E2E2),
+};
+
 class MyApp extends StatelessWidget {
   final appConfiguration;
   MyApp(this.appConfiguration);
+
+  static const AppColors = {
+    "main": Color(0xffffffff),
+    "appbar_background": Color(0xffffffff),
+  };
 
   @override
   Widget build(BuildContext context) {
--- lib/pages/tab_1/dep_0_main/Tab1Main.screen.dart
+++ lib/pages/tab_1/dep_0_main/Tab1Main.screen.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:counseling_flutter/main.dart';
 import 'package:flutter/material.dart';
 
 class Tab1MainScreen extends StatefulWidget {
@@ -28,36 +29,45 @@ class _Tab1MainScreenState extends State<Tab1MainScreen>
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Image.asset(
-      //     'assets/images/tennist_logo.png',
-      //     scale: 2.2,
-      //   ),
-      //   backgroundColor: const Color(0xff141414), //앱바 색상
-      //   elevation: 0.0, //앱바의 떠있는 효과(그림자) 0.0 은 앱바 그림자 지워줌
-      // ),
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(50),
-        child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Container(
-              color: const Color(0xff141414),
-            ),
-            Center(
-              child: Container(
-                margin: EdgeInsets.only(top: 30.0),
-                decoration: BoxDecoration(
-                  image: new DecorationImage(
-                    image: ExactAssetImage(
-                      'assets/images/tennist_logo.png',
-                      scale: 2.5,
-                    ),
-                  ),
-                ),
+        preferredSize: Size.fromHeight(45.0),
+        child: Container(
+          alignment: FractionalOffset.centerRight,
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: AppColors['appbar_border'],
+                width: 1.5,
+                style: BorderStyle.solid,
               ),
             ),
-          ],
+          ),
+          child: AppBar(
+            elevation: 0.0,
+            bottomOpacity: 0.0,
+            automaticallyImplyLeading: false, //왼쪽 화살표 뒤로 없애기
+            backgroundColor: AppColors['appbar_background'],
+            title: Text(
+              '상담사 찾기',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                letterSpacing: 0.07,
+                color: AppColors['appbar_title'],
+                fontSize: 22.0,
+              ),
+            ),
+            centerTitle: true,
+            actions: <Widget>[
+              new IconButton(
+                icon: new Icon(
+                  Icons.search,
+                  size: 30.0,
+                  color: AppColors['appbar_icon'],
+                ),
+                onPressed: () => Navigator.of(context).pop(null),
+              ),
+            ],
+          ),
         ),
       ),
       body: loading
@@ -65,16 +75,261 @@ class _Tab1MainScreenState extends State<Tab1MainScreen>
               child: CircularProgressIndicator(),
             )
           : ListView.builder(
-              padding: const EdgeInsets.all(8),
+              // padding: const EdgeInsets.all(8),
               itemCount: entries.length,
-              itemExtent: 90,
+              // itemExtent: 90,
               itemBuilder: (BuildContext context, int index) {
-                // return Container(
-                //   height: 100,
-                //   color: Colors.amber[colorCodes[index]],
-                //   child: Center(child: Text('Entry ${entries[index]}')),
-                // );
+                if (index == 0) {
+                  return Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      height: 35,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Expanded(
+                            child: Container(
+                              decoration: ShapeDecoration(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(4),
+                                  ),
+                                  side: BorderSide(
+                                    width: 1.4,
+                                    color: AppColors['btn_border_gray'],
+                                  ),
+                                ),
+                              ),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        child: Text(
+                                          "고민키워드",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontSize: 15.0,
+                                            fontWeight: FontWeight.w600,
+                                            letterSpacing: 0.07,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(2.0),
+                                    child: Container(
+                                      child: new Icon(
+                                        Icons.keyboard_arrow_down,
+                                        size: 20.0,
+                                        color: const Color(0xff898989),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Expanded(
+                            child: Container(
+                              decoration: ShapeDecoration(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(4),
+                                  ),
+                                  side: BorderSide(
+                                    width: 1.4,
+                                    color: AppColors['btn_border_gray'],
+                                  ),
+                                ),
+                              ),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Container(
+                                        child: Text(
+                                          "상담 유형",
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontSize: 15.0,
+                                            fontWeight: FontWeight.w600,
+                                            letterSpacing: 0.07,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(2.0),
+                                    child: Container(
+                                      child: new Icon(
+                                        Icons.keyboard_arrow_down,
+                                        size: 20.0,
+                                        color: const Color(0xff898989),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 12.0,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Container(
+                              child: new Icon(
+                                Icons.sort,
+                                size: 30.0,
+                                color: AppColors['appbar_icon'],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                if (index == 1) {
+                  return Container(
+                    color: const Color(0xffEBBBAE),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("심리 주치의"),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("24시간 무제한 심리상담 "),
+                              Text("가능한 상담사 보러가기 "),
+                              new Icon(
+                                Icons.keyboard_arrow_right,
+                                size: 18.0,
+                                color: AppColors['appbar_icon'],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                if (index == 2) {
+                  return Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Text(
+                                    "검색 결과 50건",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      letterSpacing: 0.05,
+                                      fontSize: 14.0,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 4.0,
+                                  ),
+                                  new Icon(
+                                    Icons.help_outline,
+                                    size: 22.0,
+                                    color: const Color(0xff848484),
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                // color: Colors.purple,
 
+                                child: Row(
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 8),
+                                      child: Text(
+                                        "고민 연관순",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          color: const Color(0xffCDCDCD),
+                                          letterSpacing: 0.05,
+                                          fontSize: 14.0,
+                                        ),
+                                      ),
+                                    ),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        border: Border(
+                                          left: BorderSide(
+                                            //                   <--- left side
+                                            color: const Color(0xffCDCDCD),
+                                            width: 1.5,
+                                          ),
+                                          right: BorderSide(
+                                            //                    <--- top side
+                                            color: const Color(0xffCDCDCD),
+                                            width: 1.5,
+                                          ),
+                                        ),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 8),
+                                        child: Row(
+                                          children: <Widget>[
+                                            Text(
+                                              "가격순",
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.w500,
+                                                color: const Color(0xffCDCDCD),
+                                                letterSpacing: 0.05,
+                                                fontSize: 14.0,
+                                              ),
+                                            ),
+                                            new Icon(
+                                              Icons.unfold_more,
+                                              size: 16.0,
+                                              color: const Color(0xff848484),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8),
+                                      child: Text(
+                                        "만족도순",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          letterSpacing: 0.05,
+                                          fontSize: 14.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }
                 return Column(
                   children: <Widget>[
                     ListTile(
